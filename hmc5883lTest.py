7fced69e2e3b16f5136ae422dee7bbb46f19aeaa
ThreeSixes <ThreeSixes@users.noreply.github.com>
2015-03-09 11:07:39 -0400

    Update hmc5883lTest.py
    Updated comments to include examples of setting the registers without using the less-efficient individual setting change methods.

--- hmc5883lTest.py
+++ hmc5883lTest.py
@@ -7,6 +7,10 @@ from pprint import pprint
 magSens = hmc5883l()
 
 # Test everything related to config register A.
+
+# Changing register A could be more efficiently achieved using this:
+# magSens.setReg(magSens.regCfgA, (magSens.avg1 | magSens.freq15 | magSens.biasNone))
+# Test the methods that individually set all register A's settings:
 regA = magSens.getReg(magSens.regCfgA)
 magSens.setSamplingAverage(magSens.avg1)
 magSens.setOutputFreq(magSens.freq15)
@@ -14,11 +18,19 @@ magSens.setBias(magSens.biasNone)
 print("Reg A:    " + hex(regA) + " -> " + hex(magSens.getReg(magSens.regCfgA)))
 
 # Test everything related to config register B.
+
+# Changing register B could be more efficiently achieved using this:
+# magSens.setReg(magSens.regCfgB, magSens.gain230)
+# Test the methods that individually set all register B's settings:
 regB = magSens.getReg(magSens.regCfgB)
 magSens.setGain(magSens.gain230)
 print("Reg B:    " + hex(regB) + " -> " + hex(magSens.getReg(magSens.regCfgB)))
 
 # Test everything related to mode register.
+
+# Changing the mode register could be more efficiently achieved using this:
+# magSens.setReg(magSens.regMode, magSens.modeCont)
+# Test the methods that individually set all register A's settings:
 regMode = magSens.getReg(magSens.regMode)
 magSens.setMode(magSens.modeCont)
 print("Reg Mode: " + hex(regMode) + " -> " + hex(magSens.getReg(magSens.regMode)))
